b58fa0755e6574dad4c3b8703995f2d80124a9b2
Sueli <suelifukuda@yahoo.com>
2018-02-01 16:46:48 -0200

    novo arquivo com alteração do antigo

--- Teste3.java
+++ Teste3.java
@@ -1,4 +1,4 @@
-public class Teste2{
+public class Teste3{
 
 	public static void main(String[] args){
 		System.out.println("Exemplo de teste de uso do git");
@@ -7,8 +7,5 @@ public class Teste2{
 
 	public static String retornaHello(){
 		return "Hello Teste";
-	}
-	
-	public static String retornaHi(){
 	}
 }
