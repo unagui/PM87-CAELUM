7633cf25bfe8630cd52897a7132c10e09cb9d011
Sueli <suelifukuda@yahoo.com>
2018-02-01 16:15:16 -0200

    novo arquivo

---
+++ Teste2.java
@@ -0,0 +1,11 @@
+public class Teste2{
+
+	public static void main(String[] args){
+		System.out.println("Exemplo de teste de uso do git");
+		System.out.println("Exemplo segundo ");
+	}
+
+	public static String retornaHello(){
+		return "Hello Teste";
+	}
+}
